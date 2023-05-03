f73a83b380eb26f050b6c5690483c91504fdd3d0
YunYouJun <me@yunyoujun.cn>
2023-05-03 17:42:42 +0800

    fix: sort and only one key

--- packages/filter-images/src/index.ts
+++ packages/filter-images/src/index.ts
@@ -1,6 +1,7 @@
 import path from 'node:path'
 import fs from 'fs-extra'
 import consola from 'consola'
+import * as pc from 'picocolors'
 import { config } from './config'
 
 /**
@@ -17,7 +18,7 @@ function getKeywords(filename: string) {
   const matches = filename.match(regex)
   if (matches) {
     const keywords = matches[1].split('_')
-    return keywords.map(i => simplifyName(i))
+    return keywords.map(i => simplifyName(i)).sort()
   }
   else {
     consola.error('No matches found', filename)
@@ -31,7 +32,7 @@ export async function main() {
     .map(i =>
       i.split(',').map(j =>
         simplifyName(j),
-      ),
+      ).sort(),
     )
 
   const files = await fs.readdir(config.sourceFolder)
@@ -42,13 +43,16 @@ export async function main() {
 
   const matchedKeywords: string[][] = []
 
-  files.forEach(async (filename) => {
+  let order = 1
+  files.forEach(async (filename, i) => {
     const keywords = getKeywords(filename)
-    const matched = words.some(i => i.includes(keywords[0]) && i.includes(keywords[1]))
+    const matched = words.some(i => i.join(',') === keywords.join(','))
 
     if (matched) {
       // Remove the matched word
-      words = words.filter(i => !(i.includes(keywords[0]) && i.includes(keywords[1])))
+      words = words.filter((i) => {
+        return !(i.join(',') === keywords.join(','))
+      })
 
       // Check if the keywords are already matched
       // if (matchedKeywords.some(i => i.includes(keywords[0]) && i.includes(keywords[1]))) {
@@ -61,15 +65,17 @@ export async function main() {
       // }
 
       matchedKeywords.push(keywords)
+      // console.log(i, order, keywords)
 
-      consola.success('Matched', filename)
+      consola.success(pc.cyan(i), 'Matched', pc.green(keywords.toString()), filename)
       await fs.copyFile(
         path.resolve(config.sourceFolder, filename),
         path.resolve(config.matchedFolder, filename),
       )
+      order += 1
     }
     else {
-      consola.error('Not matched', filename)
+      consola.error(pc.cyan(i), 'Not matched', pc.yellow(keywords.toString()), filename)
       await fs.copyFile(
         path.resolve(config.sourceFolder, filename),
         path.resolve(config.notMatchedFolder, filename),
